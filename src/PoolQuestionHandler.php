f61d974c07892c7329b0d2d8297ccf7753754ba6
Andy Truong <thehongtt@gmail.com>
2014-12-23 10:02:19 +0700

    Initial commit.

---
+++ src/PoolQuestionHandler.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Drupal\quizz_pool;
+
+use Drupal\quizz_question\Entity\Question;
+use Drupal\quizz_question\QuestionHandler;
+use Drupal\quizz\Entity\Result;
+
+/**
+ * Extension of QuizQuestion.
+ */
+class PoolQuestionHandler extends QuestionHandler {
+
+  public function delete($single_revision = FALSE) {
+    parent::delete($single_revision);
+    $query = db_delete('quiz_pool_user_answers');
+    $query->condition('question_qid', $this->question->qid);
+    if ($single_revision) {
+      $query->condition('question_vid', $this->question->vid);
+    }
+    $query->execute();
+  }
+
+  public function load() {
+    $properties = parent::load();
+
+    if (empty($this->question->field_question_reference['und'])) {
+      return $properties;
+    }
+
+    // Referenced question maybe deleted. Remove them if it was
+    /// @TODO: This should be resolved at entityreference module
+    $ref_items = field_get_items('quiz_question', $this->question, 'field_question_reference');
+    $this->question->field_question_reference['und'] = array();
+    $field_items = &$this->question->field_question_reference['und'];
+    foreach ($ref_items as $ref_item) {
+      if ($ref_question = quiz_question_entity_load($ref_item['target_id'])) {
+        $field_items[]['target_id'] = $ref_item['target_id'];
+      }
+    }
+
+    return $properties;
+  }
+
+  /**
+   * Implementation of getEntityView
+   * @see QuestionHandler#getEntityView()
+   */
+  public function view() {
+    $build = parent::view();
+    $wrapper = entity_metadata_wrapper('quiz_question', $this->question);
+
+    /* @var $sub_question Question */
+    $markup = '';
+    foreach ($wrapper->field_question_reference->getIterator() as $sub_wrapper) {
+      $sub_question = $sub_wrapper->value();
+      if ($content = $sub_question->getHandler()->view() && !empty($content['answer'])) {
+        $markup .= "<h3>{$sub_question->title}</h3>";
+        $markup .= $content['answer']['#markup'];
+      }
+    }
+    $build['answers']['#markup'] = $markup;
+    return $build;
+  }
+
+  private function getCurrentQuestion($quiz_id, $retry = FALSE) {
+    $session = &$_SESSION['quiz'][$quiz_id];
+    $key = "pool_{$this->question->qid}";
+    if (!isset($_SESSION['quiz'][$quiz_id][$key])) {
+      $_SESSION['quiz'][$quiz_id][$key] = array('passed' => FALSE, 'delta' => 0);
+    }
+    $wrapper = entity_metadata_wrapper('quiz_question', $this->question);
+    $delta = &$session[$key]['delta'];
+
+    if ($retry) {
+      $delta++;
+      if (!isset($wrapper->field_question_reference[$delta])) {
+        $delta = 0;
+      }
+      drupal_goto($_GET['q']);
+    }
+
+    return $wrapper->field_question_reference[$delta]->value();
+  }
+
+  /**
+   * Generates the question form.
+   *
+   * This is called whenever a question is rendered, either
+   * to an administrator or to a quiz taker.
+   */
+  public function getAnsweringForm(array $form_state = NULL, $result_id) {
+    $quiz = quizz_result_load($result_id)->getQuiz();
+    $retry = $quiz->repeat_until_correct && !empty($_GET['retry']);
+    $question = $this->getCurrentQuestion($quiz->qid, $retry);
+    $form = array();
+    $form[$question->qid] = $question->getHandler()->getAnsweringForm($form_state, $result_id);
+
+    if ($quiz->repeat_until_correct) {
+      $form['navigation']['pool_retry'] = array(
+          '#type'   => 'markup',
+          '#markup' => t('Try an <a href="!url">other question</a>', array(
+              '!url' => url($_GET['q'], array('query' => array('retry' => 1)))
+          )),
+          '#weight' => 50,
+      );
+    }
+
+    return $form;
+  }
+
+  /**
+   * Implementation of getMaximumScore.
+   * @see QuizQuestion#getMaximumScore()
+   */
+  public function getMaximumScore() {
+    $score = 0;
+    $wrapper = entity_metadata_wrapper('quiz_question', $this->question);
+    /* @var $question Question */
+    foreach ($wrapper->field_question_reference->getIterator() as $wrapper_question) {
+      if ($question = $wrapper_question->value()) {
+        $score += $question->getHandler()->getMaximumScore();
+      }
+    }
+    return $score;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onRepeatUntiCorrect(Result $result, array &$element) {
+    $msg = t('The answer was incorrect. Please try again.');
+    $msg .= ' ' . t('You can try with <a href="!url">another question</a>.', array(
+          '!url' => url($_GET['q'], array('query' => array('retry' => 1)))
+    ));
+    return parent::onRepeatUntiCorrect($result, $element, $msg);
+  }
+
+}
---
+++ src/PoolResponseHandler.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Drupal\quizz_pool;
+
+use Drupal\quizz_question\Entity\Question;
+use Drupal\quizz_question\ResponseHandler;
+
+/**
+ * Extension of QuizQuestionResponse
+ */
+class PoolResponseHandler extends ResponseHandler {
+
+  /**
+   * {@inheritdoc}
+   * @var string
+   */
+  protected $base_table;
+  protected $user_answer_ids;
+  protected $choice_order;
+  protected $need_evaluated;
+
+  public function __construct($result_id, Question $question, $input = NULL) {
+    parent::__construct($result_id, $question, $input);
+
+    if (NULL === $input) {
+      if ($response = $this->getCorrectAnswer()) {
+        $this->answer = $response->answer;
+        $this->score = $response->score;
+      }
+    }
+    else {
+      $this->setAnswerInput($input);
+    }
+
+    $quiz_id = $this->result->getQuiz()->qid;
+    if (!isset($_SESSION['quiz'][$quiz_id]["pool_{$this->question->qid}"])) {
+      $_SESSION['quiz'][$quiz_id]["pool_{$this->question->qid}"] = array(
+          'passed' => FALSE,
+          'delta'  => 0,
+      );
+    }
+  }
+
+  public function setAnswerInput($input) {
+    if (NULL !== $input && is_array($input)) {
+      $input = reset($input);
+    }
+    parent::setAnswerInput($input);
+  }
+
+  /**
+   * Implementation of getCorrectAnswer
+   */
+  public function getCorrectAnswer() {
+    return db_query('SELECT answer, score'
+        . ' FROM {quiz_pool_user_answers}'
+        . ' WHERE question_vid = :qvid AND result_id = :rid', array(
+          ':qvid' => $this->question->vid,
+          ':rid'  => $this->result_id
+      ))->fetch();
+  }
+
+  public function isValid() {
+    if (2 == $this->answer) { // @TODO Number 2 here is too magic.
+      drupal_set_message(t("You haven't completed the quiz pool"), 'warning');
+      return FALSE;
+    }
+    return parent::isValid();
+  }
+
+  /**
+   * @return Question
+   */
+  private function getQuestion() {
+    $quiz_id = $this->result->getQuiz()->qid;
+    $key = "pool_{$this->question->qid}";
+
+    if (!empty($_SESSION['quiz'][$quiz_id][$key])) {
+      $sess = $_SESSION['quiz'][$quiz_id][$key];
+      $passed = isset($sess['passed']) ? $sess['passed'] : FALSE;
+      $delta = isset($sess['delta']) ? $sess['delta'] : 0;
+      return entity_metadata_wrapper('quiz_question', $this->question)
+          ->field_question_reference[$passed ? $delta - 1 : $delta]
+          ->value();
+    }
+
+    $question_vid = db_select('quiz_pool_user_answers_questions', 'p')
+      ->fields('p', array('question_vid'))
+      ->condition('pool_qid', $this->question->qid)
+      ->condition('pool_vid', $this->question->vid)
+      ->condition('result_id', $this->result_id)
+      ->execute()
+      ->fetchColumn();
+
+    if (!empty($question_vid)) {
+      return $question = quiz_question_entity_load(NULL, $question_vid);
+    }
+  }
+
+  /**
+   * Implementation of save
+   * @see QuizQuestionResponse#save()
+   */
+  public function save() {
+    $sess = &$_SESSION['quiz'][$this->result->getQuiz()->qid]["pool_{$this->question->qid}"];
+    $passed = &$sess['passed'];
+    $delta = &$sess['delta'];
+
+    $wrapper = entity_metadata_wrapper('quiz_question', $this->question);
+    if ($question = $wrapper->field_question_reference[$delta]->value()) {
+      $result = $this->evaluateQuestion($question);
+      if ($result->is_valid) {
+        $passed = $result->is_correct ? TRUE : $passed;
+        if ($delta < $wrapper->field_question_reference->count()) {
+          $delta++;
+        }
+      }
+    }
+
+    if (!$passed) {
+      $question = $wrapper->field_question_reference[$delta - 1]->value();
+    }
+
+    db_insert('quiz_pool_user_answers')
+      ->fields(array(
+          'question_qid' => $this->question->qid,
+          'question_vid' => $this->question->vid,
+          'result_id'    => $this->result_id,
+          'score'        => (int) $this->getScore(),
+          'answer'       => (int) $this->answer,
+      ))
+      ->execute();
+  }
+
+  private function evaluateQuestion(Question $question) {
+    $handler = $question->getResponseHandler($this->result_id, $this->answer);
+    $answer = $handler->toBareObject();
+
+    // If a result_id is set, we are taking a quiz.
+    if (isset($this->answer)) {
+      $keys = array(
+          'pool_qid'     => $this->question->qid,
+          'pool_vid'     => $this->question->vid,
+          'question_qid' => $question->qid,
+          'question_vid' => $question->vid,
+          'result_id'    => $this->result->result_id,
+      );
+
+      db_merge('quiz_pool_user_answers_questions')
+        ->key($keys)
+        ->fields($keys + array(
+            'answer'       => serialize($this->answer),
+            'is_correct'   => $answer->is_correct,
+            'is_evaluated' => (int) $handler->isEvaluated(),
+            'score'        => (int) $handler->score(),
+        ))
+        ->execute()
+      ;
+    }
+
+    // fix error with score
+    if ($this->result->score < 0) {
+      $this->result->score = 0;
+    }
+
+    return $answer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function delete() {
+    parent::delete();
+
+    // The quiz question delete and resave instead update
+    // We have a difference between $respone update and delete.
+    // Question update $this->question is entity
+    // Question delete $this->question is custom object.
+    if (!isset($this->question->created)) {
+      db_delete('quiz_pool_user_answers_questions')
+        ->condition('pool_qid', $this->question->qid)
+        ->condition('pool_vid', $this->question->vid)
+        ->condition('result_id', $this->result_id)
+        ->execute();
+    }
+  }
+
+  /**
+   * Implementation of score
+   * @return int
+   * @see QuizQuestionResponse#score()
+   */
+  public function score() {
+    return $this->isCorrect() ? $this->getQuestionMaxScore() : 0;
+  }
+
+  public function isCorrect() {
+    $handler = $this->getQuestion()->getResponseHandler($this->result_id, $this->answer);
+    $handler->setAnswerInput($this->answer);
+    return $handler->isCorrect();
+  }
+
+  /**
+   * Implementation of getFeedbackValues.
+   */
+  public function getFeedbackValues() {
+    if (!$question = $this->getQuestion()) {
+      return array('#markup' => t('No question passed.'));
+    }
+    return $question->getResponseHandler($this->result_id)->getFeedbackValues();
+  }
+
+}
